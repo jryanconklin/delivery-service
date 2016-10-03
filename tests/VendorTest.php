bfe6868dbf212aa94effc66c4fffc433d8083540
J. Ryan Conklin <jryanconklin@gmail.com>
2016-10-03 15:18:42 -0700

    Fix merge conflict on VendorTest.

--- tests/VendorTest.php
+++ tests/VendorTest.php
@@ -1,39 +1,4 @@
 <?php
-<<<<<<< HEAD
-// 
-//     /**
-//     * @backupGlobals disabled
-//     * @backupStaticAttributes disabled
-//     */
-//
-//     require_once __DIR__."/../inc/ConnectionTest.php";
-//     require_once __DIR__."/../src/FirstClass.php";
-//     require_once __DIR__."/../src/SecondClass.php";
-//
-//     class ClassTest extends PHPUnit_Framework_TestCase
-//     {
-//         protected function tearDown()
-//         {
-//             FirstClass::deleteAll();
-//             SecondClass::deleteAll();
-//         }
-//
-//         function test_someFunction()
-//         {
-//             //Arrange
-//
-//
-//             //Act
-//
-//
-//             //Assert
-//
-//
-//        }
-//
-// //End Test
-//     }
-=======
 
     /**
     * @backupGlobals disabled
@@ -552,21 +517,6 @@
         }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //End Test
     }
->>>>>>> master
 ?>
