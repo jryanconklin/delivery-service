4534b6d11ec281eb2422f9bcdb560c13e5589383
J. Ryan Conklin <jryanconklin@gmail.com>
2016-10-05 17:02:33 -0700

    Update Database Schema and Add Data to Types, Addresses and Vendors.

--- app/app.php
+++ app/app.php
@@ -1,11 +1,11 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__."/../inc/Connection.php";
-    require_once __DIR__.'/../src/Address.php';
-    require_once __DIR__."/../src/Rider.php";
-    require_once __DIR__."/../src/Service.php";
-    require_once __DIR__."/../src/Vendor.php";
+    // require_once __DIR__."/../inc/Connection.php";
+    // require_once __DIR__.'/../src/Address.php';
+    // require_once __DIR__."/../src/Rider.php";
+    // require_once __DIR__."/../src/Service.php";
+    // require_once __DIR__."/../src/Vendor.php";
 
 //Setup
     $app = new Silex\Application();
